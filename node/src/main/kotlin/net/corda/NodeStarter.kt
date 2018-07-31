80411e6ca8d1bf93aeda2bac52edc6db6b63686c
Michele Sollecito <michele.sollecito@r3.com>
2018-07-31 11:26:18 +0100

    Trimmed build files.

--- node/src/main/kotlin/net/corda/NodeStarter.kt
+++ node/src/main/kotlin/net/corda/NodeStarter.kt
@@ -4,11 +4,8 @@ import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
-// TODO remove ComponentScan and move it to SpringBootApplication
-@ComponentScan("net.corda")
+@SpringBootApplication(scanBasePackages = ["net.corda"])
 private open class NodeStarter
 
 internal fun main(args: Array<String>) {
