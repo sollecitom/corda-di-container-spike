281a2717d4e3e38edf54940ca3f2808d0e3db179
Michele Sollecito <michele.sollecito@r3.com>
2018-08-02 10:19:30 +0100

    Final touches here and there.

--- node/src/main/kotlin/net/corda/node/auditing/LoggingAuditingService.kt
+++ node/src/main/kotlin/net/corda/node/auditing/LoggingAuditingService.kt
@@ -1,6 +1,6 @@
 package net.corda.node.auditing
 
-import net.corda.commons.events.Event
+import net.corda.commons.events.only
 import net.corda.commons.logging.loggerFor
 import net.corda.node.RpcServerStub
 import net.corda.node.api.cordapp.CordappsLoader
@@ -9,7 +9,6 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import javax.inject.Inject
 import javax.inject.Named
-import kotlin.reflect.KClass
 
 @Named
 internal class LoggingAuditingService @Inject internal constructor(bus: EventBus) {
@@ -17,21 +16,12 @@ internal class LoggingAuditingService @Inject internal constructor(bus: EventBus
     private companion object {
 
         private val logger = loggerFor<LoggingAuditingService>()
-        private val auditableEventTypes: Set<KClass<out Event>> = setOf(RpcServerStub.Event.Invocation::class, CordappsLoader.Event.CordappsWereLoaded::class)
     }
 
     init {
-        bus.events.filter { event -> auditableEventTypes.any { it.isInstance(event) } }.doOnNext { event -> log(event) }.subscribe()
-    }
-
-    private fun log(event: Event) {
-
-        when (event) {
-            is RpcServerStub.Event.Invocation -> logRpcInvocation(event)
-            is CordappsLoader.Event.CordappsWereLoaded -> logCordappsLoaded(event)
-            else -> {
-                logger.info("Event of type '${event.javaClass.name}' and ID '${event.id}' happened at ${LocalDateTime.ofInstant(event.createdAt, ZoneId.systemDefault())}.")
-            }
+        with(bus) {
+            events.only<RpcServerStub.Event.Invocation>().doOnNext(::logRpcInvocation).subscribe()
+            events.only<CordappsLoader.Event.CordappsWereLoaded>().doOnNext(::logCordappsLoaded).subscribe()
         }
     }
 
