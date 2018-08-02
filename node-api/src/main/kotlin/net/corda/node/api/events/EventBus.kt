b7e39fe624654c958bd45c8bd49920242db9121d
Michele Sollecito <michele.sollecito@r3.com>
2018-08-02 11:48:26 +0100

    Final touches here and there.

--- node-api/src/main/kotlin/net/corda/node/api/events/EventBus.kt
+++ node-api/src/main/kotlin/net/corda/node/api/events/EventBus.kt
@@ -3,7 +3,6 @@ package net.corda.node.api.events
 import net.corda.commons.events.Event
 import reactor.core.publisher.Flux
 
-// TODO try and remove this?
 interface EventBus {
 
     val events: Flux<out Event>
